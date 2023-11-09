0dc52083e4fded3f1df447c1404e31cb4549ff62
aalexebr <alex.ebr@icloud.com>
2023-11-09 16:45:08 +0100

    'add'

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,7 +1,27 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
+	
 	public static void main(String[] args) {
-		System.out.println("Hola, World");
+		System.out.println("test");
+		
+		double pricePerKm = 0.21;
+		int discountMinors = 80;
+		int discountOvers = 60;
+		
+		String userAge;
+		String userDistance;
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Your Age as Number: ");
+		userAge = in.nextLine();
+		
+		System.out.print("Your Age as Number: ");
+		userDistance = in.nextLine();
+		
+		in.close();
 	}
 }
